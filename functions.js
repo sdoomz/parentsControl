2642d6dc0f06716d4858e350a036520459f9a7fa
sdoomz <sdoomz@gmail.com>
2014-02-03 19:50:03 +0200

    update

--- functions.js
+++ functions.js
@@ -1,65 +1,48 @@
-(function() { 
-	var badWords = [
-	'bitch', 
-	'dick', 
-	'porn', 
-	'ass', 	
-	'slut', 
-	'sluts', 
-	'anal', 
-	'fuck',
-	'fucked',
-	'fucker',
-	'fuckface',
-	'fucking', 
-	'squirt', 
-	'squirting',	
-	'cum', 
-	'cumshot', 
-	'bitch',
-	'asshole',
-	'cock',
-	'cocks',
-	'blow job',
-	'whore',
-	'shithead',
-	'dickhead',
-	'faggot',
-	'fucked-up',
-	'pussy',
-	'bastard',
-	'anus',
-	'jerk',
-	'jerking',
-	'creature',
-	'bloke',
-	'dickhead',	
-	'piss', 
-	'shit',
-	'sucker',
-	'bullshit',
-	'cunt',
-	's.o.b',
-	'fart',
-	'motherfucker',
-	'masturbation',
-	'blowjob',
-	'blowjobs',
-	'handjob',
-	'handjobs'
-	].join("|");
-	var regExp = new RegExp('\\b('+badWords+')\\b', "gi");  
+(function() {
+	var LANG = 'eng';
+	var DATA = null;
+	function getXmlHttp(){
+		var xmlhttp;
+		try {
+		    xmlhttp = new ActiveXObject("Msxml2.XMLHTTP");
+		} catch (e) {
+			try {
+			    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+			} catch (E) {
+			    xmlhttp = false;
+			}
+		}
+		if (!xmlhttp && typeof XMLHttpRequest!='undefined') {
+		   xmlhttp = new XMLHttpRequest();
+		}
+		return xmlhttp;
+	}
 
-	var walker = document.createTreeWalker(
-	    document.body, 
-	    NodeFilter.SHOW_TEXT, 
-	    null, 
-	    false
-	);
+	var xmlhttp = getXmlHttp();	
+	xmlhttp.open('GET', chrome.extension.getURL(LANG+'.json'), true);
+	xmlhttp.onreadystatechange = function() {
+	  if (xmlhttp.readyState == 4) {
+	    if(xmlhttp.status == 200) {
+	    	DATA = JSON.parse(xmlhttp.responseText);
+	    	changeWords(DATA.words.join("|")); 
+	    }
+	  }
+	};
+	xmlhttp.send(null);
 
-	var node;
+	function changeWords(badWords) {
+		var regExp = new RegExp('\\b('+badWords+')\\b', "gi");  
+		var walker = document.createTreeWalker(
+		    document.body, 
+		    NodeFilter.SHOW_TEXT, 
+		    null, 
+		    false
+		);
+		var node;
 
-	while(node = walker.nextNode()) {     
-	    node.nodeValue = node.nodeValue.replace(regExp, '****');
+		while(node = walker.nextNode()) {
+			console.log(node);
+		    node.nodeValue = node.nodeValue.replace(regExp, '****');
+		}		
 	}
 })();
