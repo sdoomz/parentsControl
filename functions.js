a30f89d2e6572e8cfa09ebf61998ea4d5bda0b44
Svyat <sdoomz@gmail.com>
2014-08-12 02:40:02 +0300

    debounce was added change input values

--- functions.js
+++ functions.js
@@ -1,5 +1,7 @@
 (function() {
 	function StorageMgr(options) {
+		console.log('StorageMgr was created succesfully');
+
 		this.name = options.name;
 	};
 
@@ -39,130 +41,175 @@
 		}
 	};
 
-	var jsonWorker = {};
-	var name = '_$webCensor';	
+	var Utils = {
+		debounce : function(fn, timeout, invokeAsap, ctx) {
+			if(arguments.length == 3 && typeof invokeAsap != 'boolean') {
+				ctx = invokeAsap;
+				invokeAsap = false;
+			}
+			var timer;
 
-	var storage = new StorageMgr({
-		name: name
-	});
+			return function() {
+				var args = arguments;
+	            ctx = ctx || this;
 
-	var defStore = storage.getSettings();
+				invokeAsap && !timer && fn.apply(ctx, args);
 
-	var defaults = {
-		language  : defStore.language  || 'eng', 
-		words     : defStore.words     || '',
-		userWords : defStore.userWords || '',
-		safeWords : defStore.safeWords || '',
-		replacer  : defStore.replacer  || '*****',
-		active    : (defStore.hasOwnProperty('active') && defStore.active != null) ? defStore.active : true
+				clearTimeout(timer);
+
+				timer = setTimeout(function() {
+					!invokeAsap && fn.apply(ctx, args);
+					timer = null;
+				}, timeout);
+
+			};
+		}
 	};
 
-	storage.saveSettings(defaults);
+	var App = {
+		storage: new StorageMgr({name: '_$webCensor'}),	
+		defaults: {
+			language  : 'eng', 
+			words     : '',
+			userWords : '',
+			safeWords : '',
+			replacer  : '*****',
+			active    : true
+		},
 
-	chrome.runtime.sendMessage({type: 'getLocal'}, function (response) {
-		var data = JSON.parse(response);
-	  	storage.setValue(data);
-	});
-
-	chrome.runtime.onMessage.addListener(optionsUpdateCallback);
-
-	function optionsUpdateCallback(request, sender, sendResponse) {
-		if (request.type == 'update') {	
-
-			storage.setValue(request.data)
-
-			if (storage.getValue('words') == null || (storage.getValue('language') != request.data.language)) {
-				jsonWorker.sendRequest();
-			} else {
-				if (storage.getValue('active') == true)
-		    		changeWords(); 
-			}		
+		sendRequest: function() { 
+			var me = this;
+			var xmlhttp = new XMLHttpRequest();
+			xmlhttp.open('GET', chrome.extension.getURL('words.json'), true);
 			
-			var reload = confirm('You may need to reload page to update changes. Are you sure you want to reload page right now?');
-
-			if (reload) {
-				window.location.reload();
-			}
-
-		}
-	    
-	}
-
-	jsonWorker.sendRequest = function() { 
-		var xmlhttp = new XMLHttpRequest();
-		xmlhttp.open('GET', chrome.extension.getURL('words.json'), true);
-		
-		xmlhttp.onreadystatechange = function() {
-			if (xmlhttp.readyState == 4) {
-		    	if(xmlhttp.status == 200) {
-		    		var data = JSON.parse(xmlhttp.responseText);   			    	
+			xmlhttp.onreadystatechange = function() {
+				if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+		    		var data = JSON.parse(xmlhttp.responseText); 
+		    		var storage = me.storage; 			    	
 		    		var list = data[storage.getValue('language')].join('|');
 
 		    		storage.setValue('words', list)
 		    		
 		    		if (storage.getValue('active') == true)
-		    			changeWords(); 
-		    	}
-		  	}
-		}
+		    			me.replaceProfanity(); 
 
-		xmlhttp.send(null);
-	};
-	
-	if ( storage.getValue('words') == '') {
-		jsonWorker.sendRequest();
-	} 
+		    		console.log('settings recived', storage);
+			  	}
+			}
 
-	function changeWords() {
-		if ( storage.getValue('active') != true) {
-			return false;
-		}	
+			xmlhttp.send(null);
+		},
 
-		var walker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, null, false);
-		var node;
-		var replacer = storage.getValue('replacer');
-		var stopWords = storage.getValue('words');
+		optionsUpdateCallback: function(request, sender, sendResponse) {
+			if (request.type == 'update') {	
+				var storage = this.storage;
+				console.log('reguest response', request.data, storage);
+				storage.setValue(request.data)
 
-		if (storage.getValue('userWords') != '') {
-			stopWords += '|' + storage.getValue('userWords');
-		}
+				if (storage.getValue('words') == null || (storage.getValue('language') != request.data.language)) {
+					this.sendRequest();
+				} else {
+					if (storage.getValue('active') == true)
+			    		this.replaceProfanity(); 
+				}		
+				
+				var reload = confirm('You may need to reload page to update changes. Are you sure you want to reload page right now?');
 
-		var regExp = createRegExp(stopWords.split('|'));
+				if (reload) {
+					window.location.reload();
+				}
+			}		    
+		},
 
-		while (node = walker.nextNode()) {			
-		    node.nodeValue = node.nodeValue.replace(regExp, replacer);
-		}	
-	}
+		replaceProfanity: Utils.debounce(function() {
+			var storage = this.storage;
+			console.log('changes words');
 
-	function createRegExp (stopWords, safeWords) { 
-		var stopWords = stopWords || storage.getValue('words').split('|');
-		var safeWords = safeWords || storage.getValue('safeWords');
- 		
- 		if (safeWords.length) {
-	 		var regExp = new RegExp('\\b('+safeWords+')\\b', 'gi');  
+			if ( storage.getValue('active') != true) {
+				return false;
+			}	
 
-			for (var i = stopWords.length - 1; i >= 0; i--) {
-				if (stopWords[i].search(regExp) != -1) {
-					stopWords.splice(i, 1);
+			var walker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, null, false);
+			var node;
+			var replacer = storage.getValue('replacer');
+			var stopWords = storage.getValue('words');
+
+			if (storage.getValue('userWords') != '') {
+				stopWords += '|' + storage.getValue('userWords');
+			}
+
+			var regExp = this.createRegExp(stopWords.split('|'));
+
+			while (node = walker.nextNode()) {			
+			    node.nodeValue = node.nodeValue.replace(regExp, replacer);
+			}
+
+			this.changeInputValues(regExp, replacer);
+		}, 300),
+		
+		changeInputValues: function(regExp, replacer) {			
+			var walker = document.getElementsByTagName('input');
+			var node;
+
+			for (var i = walker.length - 1; i >= 0; i--) {
+				if (walker[i].value && typeof walker[i].value == 'string') {					
+					walker[i].value = walker[i].value.replace(regExp, replacer);
 				}
 			};
+
+			return this;		
+		},	
+
+		createRegExp: function(stopWords, safeWords) { 
+			var stopWords = stopWords || this.storage.getValue('words').split('|');
+			var safeWords = safeWords || this.storage.getValue('safeWords');
+	 		
+	 		if (safeWords.length) {
+		 		var regExp = new RegExp('\\b('+safeWords+')\\b', 'gi');  
+
+				for (var i = stopWords.length - 1; i >= 0; i--) {
+					if (stopWords[i].search(regExp) != -1) {
+						stopWords.splice(i, 1);
+					}
+				};
+			}
+
+			return new RegExp('\\b('+stopWords.join('|')+')\\b', 'gi');
+		}, 
+
+		attachHandlers: function() {
+			var me = this;
+
+			chrome.runtime.sendMessage({type: 'getLocal'}, function (response) {
+				var data = JSON.parse(response);
+			  	me.storage.setValue(data);
+			});
+
+			chrome.runtime.onMessage.addListener(this.optionsUpdateCallback.bind(this));
+
+			document.addEventListener("DOMSubtreeModified", function() {
+				console.log('DOM modifiedl');
+				me.replaceProfanity();
+			}, false);
+		},
+
+		init: function() {
+			var defStore = this.storage.getSettings();			
+			var defaults = this.defaults;
+			var me = this;
+			
+			for (var prop in defaults) {
+				if (prop in defStore) defaults[prop] = defStore[prop];				
+			}
+		
+			this.storage.saveSettings(defaults);
+			this.attachHandlers();
+
+			if ( App.storage.getValue('words') == '') {
+				App.sendRequest();
+			} 
 		}
+	};
 
-		return new RegExp('\\b('+stopWords.join('|')+')\\b', 'gi');
-	} 
-	
-	document.addEventListener("DOMSubtreeModified", refreshDomAjax, false);
-
-	var busy = false; 
-
-	function refreshDomAjax(e) {		
-		if(!busy && (storage.getValue('active') == true))  {
-			setTimeout(function() { 
-				changeWords();
-				busy = false;
-			}, 700);
-		}
-		busy = true;
-	}
-
+	App.init();
 })();
